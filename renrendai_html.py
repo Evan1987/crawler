78a359c83a376bf6e09c47725a62fe26d691f5ff
zhaochengming <zhaochengming@yinker.com>
2017-08-09 13:06:27 +0800

    modified format Signed-off-by: zhaochengming <zhaochengming@yinker.com>

--- renrendai_html.py
+++ renrendai_html.py
@@ -7,31 +7,31 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-def getObj(url,page):
+def getObj(url, page):
     url = url+"pageIndex="+str(page)
     rep = requests.get(url)
     html = rep.text.encode(rep.encoding).decode('utf-8')
     rawObj = json.loads(html)
     dataObj = rawObj['data']
     loanObj = dataObj['loans']
-    index = ['loanId','title','interest','amount','months','status','beginBidTime']
+    index = ['loanId', 'title', 'interest', 'amount', 'months', 'status', 'beginBidTime']
     df = pd.DataFrame(loanObj)
-    df = df.ix[:,index]
+    df = df.ix[:, index]
     return df
 
 url = 'https://www.renrendai.com/loan'
-trueUrl ="https://www.renrendai.com/lend/loanList!json.action?"
+trueUrl = "https://www.renrendai.com/lend/loanList!json.action?"
 rep = requests.get(url=url)
 rawhtml = rep.text.encode(rep.encoding).decode('utf-8')
-soup = BeautifulSoup(rawhtml,'lxml')
-targetTag = soup.find(type = re.compile('json'))
+soup = BeautifulSoup(rawhtml, 'lxml')
+targetTag = soup.find(type=re.compile('json'))
 rawObj = json.loads(targetTag.text)
 dataObj = rawObj['data']
 totalPage = int(dataObj['totalPage'])
 result = pd.DataFrame()
 
-for page in range(1,totalPage+1):
-    tmp = getObj(trueUrl,page)
+for page in range(1, totalPage+1):
+    tmp = getObj(trueUrl, page)
     result = result.append(tmp)
 
 result.to_csv("F:/result.csv")
